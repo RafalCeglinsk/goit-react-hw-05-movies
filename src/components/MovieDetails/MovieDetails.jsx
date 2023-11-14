448b35c1b48065080ec8846802216bf84577ccb6
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2023-11-14 19:36:54 +0100

    Movies, MoviesDetails
    Added function of displaying movies by query and displaying movie details

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,16 +1,61 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Cast from '../Cast/Cast';
-import Reviews from '../Reviews/Reviews';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { getMovieDetails } from 'api/getMovieDetails';
 
 function MovieDetails() {
+  const location = useLocation();
+  const [details, setDetails] = useState(null);
+  const [movieId] = useState(location.state.movieId);
+
+  useEffect(() => {
+    async function fetchMovieDetails() {
+      try {
+        const data = await getMovieDetails(movieId);
+        setDetails(data);
+        console.log(data);
+      } catch (error) {
+        console.error('Error while fetching movie details', error);
+      }
+    }
+
+    fetchMovieDetails();
+  }, []);
+
+  if (!details) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      MovieDetails
-      <Routes>
-        <Route path="cast" element={<Cast />} />
-        <Route path="reviews" element={<Reviews />} />
-      </Routes>
+      <h2>{details.title}</h2>
+      <p>User Score: {details.vote_average}</p>
+      <h2>Overview</h2>
+      <p>{details.overview}</p>
+      <h2>Genres</h2>
+      <p>
+        {details.genres.map(genre => (
+          <span key={genre.id}>{genre.name}, </span>
+        ))}
+      </p>
+
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${details.poster_path}`}
+        alt={details.title}
+      />
+
+      <ul>
+        <p>Additional Information</p>
+        <li>
+          <Link to="cast" state={{ movieId: movieId }}>
+            Cast
+          </Link>
+        </li>
+        <li>
+          <Link to="reviews" state={{ movieId: movieId }}>
+            Reviews
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { getMoviesByQuery } from 'api/getMoviesByQuery';
 
 function Movies() {
-  return <div>Movies</div>;
+  const [query, setQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const data = await getMoviesByQuery(query);
+      setSearchResults(data.results);
+    } catch (error) {
+      console.error('Error searching movies', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Search Movies</h2>
+      <input
+        type="text"
+        value={query}
+        onChange={e => setQuery(e.target.value)}
+        placeholder="Enter a movie title..."
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      <ul>
+        {searchResults.map(movie => (
+          <li key={movie.id}>
+            {' '}
+            <Link to={`/movies/${movie.id}`} state={{ movieId: movie.id }}>
+              {movie.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Movies;
