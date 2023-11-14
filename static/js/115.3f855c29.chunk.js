2f3e64498e4c33706baa341b7b1468d31baa2e6d
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2023-11-14 15:14:20 +0000

    Deploying to gh-pages from @ RafalCeglinsk/goit-react-hw-05-movies@8d967c43696e0589026a6ab0a6d45334810bf761 🚀

---
+++ static/js/115.3f855c29.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[115],{115:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Movies"})}}}]);
+//# sourceMappingURL=115.3f855c29.chunk.js.map
--- static/js/947.665a6d04.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[947],{947:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Movie Details"})}}}]);
-//# sourceMappingURL=947.665a6d04.chunk.js.map
